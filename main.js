25f429d6c305157fc1de1fbcfff9fc921bea1830
Zylvyn <zylvyn@gmail.com>
2019-12-22 19:52:52 -0500

    Testing header

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 var gameData = {
   hp: 10,
   maxhp: 10,
-  stamina: 0,
-  maxstamina: 10,
+  stamina: 10,
+  maxStamina: 10,
   seeds: 0,
   gold: 0,
-  seedsPerClick: 1
+  seedsPerClick: 1,
+  seedsPerClickCost: 10,
+  xpDigging: 0,
+  xpFarming: 0,
 }
 
 function digSeeds() {
-  gameData.seeds += gameData.seedsPerClick
-  document.getElementById("seedsDug").innerHTML = gameData.seedsDug + " seeds"
+  if (gameData.stamina >= 1) {
+    gameData.stamina --;
+    gameData.seeds += gameData.seedsPerClick;
+    document.getElementById("seedsDug").innerHTML = gameData.seeds + " seeds";
+    document.getElementById("stamina").innerHTML = gameData.stamina + " / " + gameData.maxStamina + " stamina";
+  }
+}
+
+function buySeedsPerClick() {
+  if (gameData.gold >= gameData.seedsPerClickCost) {
+    gameData.gold -= gameData.seedsPerClickCost
+    gameData.seedsPerClick += 1
+    gameData.seedsPerClickCost *= 2
+  }
 }
